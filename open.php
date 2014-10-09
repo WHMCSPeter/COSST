09f5db2b62097a4848ee0c6dc4c02aa9e08173c1
Peter Bishop <peter@lostnerd.uk>
2014-10-09 20:48:28 +0100

    Create open.php (/open)

--- open.php
+++ open.php
@@ -1,7 +1,38 @@
 <?php
 require_once('config.php');
+$page = 'dashboard';
 if(!LOGGED_IN) {
-	header("Location: ".$system_url."login?msg=openticket");
+	header("Location: ".$system_url."login?error=restricted");
 	die();
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>Open a Ticket &raquo; <?php echo $sitename; ?></title>
+    <?php require_once('assets/inc/inc.head.php'); ?>
+  </head>
+
+  <body>
+
+    <?php require_once('assets/inc/inc.nav.php'); ?>
+
+    <div class="container">
+      
+      </div>
+      
+      <hr>
+
+      <footer>
+        <?php require_once('assets/inc/inc.footer.php'); ?>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?php echo $system_url; ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
